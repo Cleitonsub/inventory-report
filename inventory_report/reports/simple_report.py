91d82070ec7ce40831878b16c5a91bc31ee18087
Cleiton <cleitonrmacedo@gmail.com>
2022-12-15 20:00:37 -0300

    feat: Requisito 2 - Criada funcao que gera a versao simplificada do relatorio

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,19 @@
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(param: list) -> str:
+        old_fab_date = min([item['data_de_fabricacao'] for item in param])
+
+        close_val_date = min([item['data_de_validade'] for item in param])
+
+        big_stock = Counter(
+            [item['nome_da_empresa'] for item in param]
+        ).most_common(1)[0][0]
+
+        return (
+            f"Data de fabricação mais antiga: {old_fab_date}\n"
+            f"Data de validade mais próxima: {close_val_date}\n"
+            f"Empresa com mais produtos: {big_stock}"
+        )
