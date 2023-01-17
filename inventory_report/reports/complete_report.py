07b325196e24b8a01b5ac7c8925c6d3b3ca378cd
Cleiton <cleitonrmacedo@gmail.com>
2023-01-17 16:00:43 -0300

    feat: Requisito 3 - Criada funcao que gera a versao completa do relatorio

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,29 @@
+from inventory_report.reports.simple_report import SimpleReport
+
+
 class CompleteReport:
-    pass
+    @staticmethod
+    def generate(param: list) -> str:
+
+        simple_report = SimpleReport.generate(param)
+
+        company_stock = {}
+
+        for item in param:
+            if item["nome_da_empresa"] in company_stock:
+                company_stock[item["nome_da_empresa"]] += 1
+            else:
+                company_stock[item["nome_da_empresa"]] = 1
+
+        company_quantity = ""
+
+        # items retorna (chave, valor) do dicionário
+        # link: https://www.w3schools.com/python/ref_dictionary_items.asp
+        for key, value in company_stock.items():
+            company_quantity += f"- {key}: {value}\n"
+
+        return (
+            f"{simple_report}\n"
+            f"Produtos estocados por empresa:\n"
+            f"{company_quantity}"
+        )
